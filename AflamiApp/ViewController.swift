14c32ed3add6922645e2245eff4eb463ffecbaef
Ahmed Hassan <ahmdmhasn@icloud.com>
2019-04-02 23:29:43 +0200

    Initial Commit

---
+++ AflamiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AflamiApp
+//
+//  Created by Ahmed M. Hassan on 4/2/19.
+//  Copyright © 2019 Ahmed M. Hassan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
